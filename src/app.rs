a70c08bc13dd06c51f34562d666d2b320a8a8a0e
Ajay Anand <ajay.arthur@gmail.com>
2024-01-23 23:03:12 -0800

    Created plot for simple model

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,6 @@
+use crate::models::{PacejkaModels, Simple};
+use egui_plot::{Line, Plot, PlotPoints};
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
@@ -5,18 +8,11 @@ pub struct TunerApp {
     model: PacejkaModels,
 }
 
-#[derive(serde::Deserialize, serde::Serialize, Debug, PartialEq)]
-pub enum PacejkaModels {
-    Simple,
-    Lateral,
-    Longitudinal,
-}
-
 impl Default for TunerApp {
     fn default() -> Self {
         Self {
             // Example stuff:
-            model: PacejkaModels::Simple,
+            model: PacejkaModels::Simple(Simple::default()),
         }
     }
 }
@@ -71,10 +67,16 @@ impl eframe::App for TunerApp {
             // The central panel the region left after adding TopPanel's and SidePanel's
             ui.heading("Pacejka Tuner");
 
+            let label = get_model_label(&self.model);
+
             egui::ComboBox::from_label("Select a model!")
-                .selected_text(format!("{:?}", self.model))
+                .selected_text((label).to_string())
                 .show_ui(ui, |ui| {
-                    ui.selectable_value(&mut self.model, PacejkaModels::Simple, "Simple");
+                    ui.selectable_value(
+                        &mut self.model,
+                        PacejkaModels::Simple(Simple::default()),
+                        "Simple",
+                    );
                     ui.selectable_value(&mut self.model, PacejkaModels::Lateral, "Lateral '94");
                     ui.selectable_value(
                         &mut self.model,
@@ -83,7 +85,51 @@ impl eframe::App for TunerApp {
                     );
                 });
 
-            
+            ui.spacing();
+
+            let model_plot: PlotPoints = (0..500)
+                .step_by(1)
+                .map(|i| {
+                    let x = i as f64 * 0.001;
+                    [x, get_model_fit(&self.model, x)]
+                })
+                .collect();
+            let line = Line::new(model_plot);
+            Plot::new("model_plot")
+                .view_aspect(2.0)
+                .y_axis_label(get_model_axis_label(&self.model))
+                .x_axis_label("Slip Angle (rad)")
+                .show(ui, |plot_ui| plot_ui.line(line));
+
+            match self.model {
+                PacejkaModels::Simple(ref mut simple) => simple.create_sliders(ui),
+                PacejkaModels::Lateral => (),
+                PacejkaModels::Longitudinal => (),
+            };
         });
+    }
+}
+
+fn get_model_fit(model: &PacejkaModels, x: f64) -> f64 {
+    match model {
+        PacejkaModels::Simple(simple) => simple.calc_f(x),
+        PacejkaModels::Lateral => 1.0,
+        PacejkaModels::Longitudinal => 1.0,
+    }
+}
+
+fn get_model_axis_label(model: &PacejkaModels) -> String {
+    match model {
+        PacejkaModels::Simple(_) => String::from("Lateral Force (N)"),
+        PacejkaModels::Lateral => String::from("Lateral Force (N)"),
+        PacejkaModels::Longitudinal => String::from("Longituindal Force (N)"),
+    }
+}
+
+fn get_model_label(model: &PacejkaModels) -> String {
+    match model {
+        PacejkaModels::Simple(_) => String::from("Simple"),
+        PacejkaModels::Lateral => String::from("Lateral '94"),
+        PacejkaModels::Longitudinal => String::from("Longitudinal '94"),
     }
 }
---
+++ src/models.rs
@@ -0,0 +1,46 @@
+use egui::Ui;
+
+#[derive(serde::Deserialize, serde::Serialize, Debug, PartialEq)]
+pub enum PacejkaModels {
+    Simple(Simple),
+    Lateral,
+    Longitudinal,
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Debug, PartialEq)]
+pub struct Simple {
+    pub label: String,
+    fz: f64, // N
+    b: f64,
+    c: f64,
+    d: f64,
+    e: f64,
+}
+
+impl Simple {
+    pub fn default() -> Self {
+        Self {
+            label: String::from("Simple"),
+            fz: 1000.,
+            b: 10.,
+            c: 1.9,
+            d: 1.,
+            e: 0.97,
+        }
+    }
+
+    pub fn calc_f(&self, slip: f64) -> f64 {
+        self.fz
+            * self.d
+            * (self.c * (self.b * slip - self.e * (self.b * slip - (self.b * slip).atan())).atan())
+                .sin()
+    }
+
+    pub fn create_sliders(&mut self, ui: &mut egui::Ui) {
+        ui.add(egui::Slider::new(&mut self.fz, 0.0..=100000.0).text("Fz [N]"));
+        ui.add(egui::Slider::new(&mut self.b, 4.0..=12.0).text("B (Stiffness)"));
+        ui.add(egui::Slider::new(&mut self.c, 1.0..=2.0).text("C (Shape)"));
+        ui.add(egui::Slider::new(&mut self.d, 0.1..=0.9).text("D (Peak)"));
+        ui.add(egui::Slider::new(&mut self.e, -10.0..=1.0).text("E (Curvature)"));
+    }
+}
